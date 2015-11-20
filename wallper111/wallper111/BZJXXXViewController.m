05864f2e7cfb0e9fa6b956322353fc3ce3c49d57
neuedu <neuedu@neuedu.local>
2015-11-20 17:31:36 +0800

    @"娄少华"lsh

---
+++ wallper111/wallper111/BZJXXXViewController.m
@@ -0,0 +1,37 @@
+//
+//  BZJXXXViewController.m
+//  wallper111
+//
+//  Created by neuedu on 15/11/20.
+//  Copyright (c) 2015年 neuedu. All rights reserved.
+//
+
+#import "BZJXXXViewController.h"
+
+@interface BZJXXXViewController ()
+
+@end
+
+@implementation BZJXXXViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
