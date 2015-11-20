b7fe189298779a53277df36ea9b1b2951b6a18a1
neuedu <neuedu@neuedu.local>
2015-11-20 11:53:58 +0800

    lsh

--- wallper111/wallper111/BZChoiceViewController.m
+++ wallper111/wallper111/BZChoiceViewController.m
@@ -8,30 +8,51 @@
 
 #import "BZChoiceViewController.h"
 
-@interface BZChoiceViewController ()
+@interface BZChoiceViewController ()<UIScrollViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UIScrollView *JXScrollview;
+@property (weak, nonatomic) IBOutlet UIPageControl *JXpageControl;
+@property (weak, nonatomic) IBOutlet UICollectionView *JXCollectionView;
 
 @end
 
 @implementation BZChoiceViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    _JXScrollview.delegate = self;
     // Do any additional setup after loading the view.
+    self.navigationController.navigationBarHidden = YES;
+    [self setJXScrollview];
+    [self setPageControlPosition];
+  
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)setJXScrollview{
+    NSURL * url = [[NSBundle mainBundle]URLForResource:@"lunboqi.plist" withExtension:nil];
+    NSArray * GG = [NSArray arrayWithContentsOfURL:url];
+    for (int i = 0 ; i <GG.count; i++) {
+        UIImageView * GGimg = [[UIImageView alloc]initWithFrame:CGRectMake(i*_JXScrollview.frame.size.width, 0, _JXScrollview.frame.size.width, _JXScrollview.frame.size.height)];
+        [_JXScrollview addSubview:GGimg];
+       
+    }
+    _JXScrollview .contentSize = CGSizeMake(GG.count* _JXScrollview.frame.size.width, _JXScrollview.frame.size.height);
+    _JXScrollview.pagingEnabled = YES;
+}
+-(void) setPageControlPosition{
+    
+    NSURL * url = [[NSBundle mainBundle]URLForResource:@"lunboqi.plist" withExtension:nil];
+    NSArray * GG = [NSArray arrayWithContentsOfURL:url];
+    _JXpageControl.numberOfPages = GG.count;
+    _JXpageControl.currentPage = 0;
+    
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+-(void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView{
+    CGFloat num = scrollView.contentOffset.x/self.view.frame.size.width;
+    _JXpageControl.currentPage = num;
 }
-*/
+
 
 @end
